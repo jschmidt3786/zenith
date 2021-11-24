af305358340e425c78d053641fb5dea232ee783b
Jeff Schmidt <jeff.schmidt@zenith.com>
2021-11-24 11:19:46 -0600

    initial commit

---
+++ base/usr/home/jschmidt/.ssh/build_config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#set -o
+DATE=$(date +%s)
+cd ~/.ssh/
+cp config config_backup-${DATE}
+cat config.base >  config
+cat include.d/* >> config
+\diff -du config_backup-${DATE} config && \
+  rm config_backup-${DATE}
